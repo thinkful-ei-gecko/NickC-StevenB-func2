cd81f1702bcdc01d51cd58677919df59b7c38e75
najc33 <najcoletta@gmail.com>
2019-08-12 17:12:02 -0400

    decode function

--- func.js
+++ func.js
@@ -12,3 +12,22 @@ else if (num >0) {console.log('to infinity')}
 else if (num <0) {console.log('to negative infinity')}
 else {console.log('Staying Home')}
 }
+
+
+//if /else ...for loop... looping through string... string 
+//input: a string.
+//output: a new string. 
+
+function decode(string){
+//given a string, take the 1st letter and return a certain index 
+//of the string based on the coding system. 
+if (string.charAt(0) === 'a') {console.log(string.charAt(1))
+}
+else if (string.charAt(0) === 'b') {console.log(string.charAt(2))
+}
+else if (string.charAt(0) === 'c') {console.log(string.charAt(3))
+}
+else if (string.charAt(0) === 'd') {console.log(string.charAt(4))
+}
+else {console.log(" ")}
+}
